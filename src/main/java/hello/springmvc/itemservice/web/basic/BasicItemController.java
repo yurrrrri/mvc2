b927fd0f749074b7f8b495d9122e4d00c624bd59
yurison <yu_ri_son@naver.com>
2023-11-15 20:02:50 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,8 +37,20 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item(itemName, price, quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(Item item) { //@ModelAttribute("item") 생략
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     // 테스트용 데이터 추가
