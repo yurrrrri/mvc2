cdb1c91620d4679e22d51130a0e3499c6045d053
yurison <yu_ri_son@naver.com>
2023-11-22 14:54:40 +0900

    Init - 메시지, 국제화

--- src/main/java/hello/springmvc/itemservice/domain/item/Item.java
+++ src/main/java/hello/springmvc/itemservice/domain/item/Item.java
@@ -3,8 +3,6 @@ package hello.springmvc.itemservice.domain.item;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 public class Item {
@@ -13,11 +11,6 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
-
-    private Boolean open; // 판매 여부
-    private List<String> regions; // 등록 지역
-    private ItemType itemType; // 상품 종류
-    private String deliveryCode; // 배송 방식
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
