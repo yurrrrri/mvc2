489bad027ec5cad017cfb354bd0c0c50af6e6c11
yurison <yu_ri_son@naver.com>
2023-12-20 23:36:05 +0900

    Bean Validation - 시작

--- src/main/java/hello/springmvc/itemservice/domain/item/Item.java
+++ src/main/java/hello/springmvc/itemservice/domain/item/Item.java
@@ -1,15 +1,27 @@
 package hello.springmvc.itemservice.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 @NoArgsConstructor
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item(String itemName, Integer price, Integer quantity) {
---
+++ src/test/java/hello/springmvc/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,30 @@
+package hello.springmvc.itemservice.validation;
+
+import hello.springmvc.itemservice.domain.item.Item;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+class BeanValidationTest {
+
+    @Test
+    void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation.getMessage() = " + violation.getMessage());
+        }
+    }
+}
