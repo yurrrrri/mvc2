6960d6b17ac94d895030bf51287116ca9c18afa5
yurison <yu_ri_son@naver.com>
2023-12-21 00:14:37 +0900

    Bean Validation - 오브젝트 오류

--- src/main/java/hello/springmvc/itemservice/domain/item/Item.java
+++ src/main/java/hello/springmvc/itemservice/domain/item/Item.java
@@ -6,7 +6,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
+// 권장하지 않음
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 가격은 10,000원 이상이어야 합니다.")
 @Data
 @NoArgsConstructor
 public class Item {
