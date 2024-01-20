7b3bc41d426fe8fca30a2ce614e157ab7f4ab170
yurison <yu_ri_son@naver.com>
2024-01-20 21:32:25 +0900

    회원 가입

--- src/main/java/hello/springmvc/TestDataInit.java
+++ src/main/java/hello/springmvc/TestDataInit.java
@@ -2,6 +2,8 @@ package hello.springmvc;
 
 import hello.springmvc.itemservice.domain.item.Item;
 import hello.springmvc.itemservice.domain.item.ItemRepository;
+import hello.springmvc.itemservice.domain.member.Member;
+import hello.springmvc.itemservice.domain.member.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,10 +13,17 @@ import org.springframework.stereotype.Component;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("tester");
+        memberRepository.save(member);
     }
 }
---
+++ src/main/java/hello/springmvc/itemservice/web/login/member/MemberController.java
@@ -0,0 +1,35 @@
+package hello.springmvc.itemservice.web.login.member;
+
+import hello.springmvc.itemservice.domain.member.Member;
+import hello.springmvc.itemservice.domain.member.MemberRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/addMemberForm";
+        }
+        memberRepository.save(member);
+
+        return "redirect:/";
+    }
+}
